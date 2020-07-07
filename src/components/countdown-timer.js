ff770d27b17d840da79208291265b8ed0ea43800
Justin Chadwell <me@jedevc.com>
2020-07-07 20:15:06 +0100

    Make timer actually countdown

--- src/components/countdown-timer.js
+++ src/components/countdown-timer.js
@@ -1,20 +1,71 @@
-import React from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
-import { RiArrowDownLine, RiArrowRightSLine } from "react-icons/ri"
+import React, { useLayoutEffect, useState } from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import { FaClock } from "react-icons/fa"
+import moment from "moment"
 
-export default function BlogListHome() {
+export default function CountdownTimer() {
+  const data = useStaticQuery(graphql`
+    query HomeLayoutQuery {
+      site {
+        siteMetadata {
+          eventStartString: eventStart
+          eventEndString: eventEnd
+        }
+      }
+    }
+  `)
+
+  const { eventStartString, eventEndString } = data.site.siteMetadata
+  const eventStart = moment(eventStartString)
+  const eventEnd = moment(eventEndString)
+
+  const [now, setNow] = useState(null)
+  useLayoutEffect(() => {
+    setNow(moment())
+    const interval = setInterval(() => {
+      setNow(moment())
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  if (now == null) {
+    return <></>
+  }
+
+  let diff
+  let text
+  if (now < eventStart) {
+    diff = moment.duration(eventStart.diff(now))
+    text = "to start"
+  } else if (now > eventStart && now < eventEnd) {
+    diff = moment.duration(eventEnd.diff(now))
+    text = "left"
+  } else {
+    diff = null
+    text = "time up!"
+  }
+
   return (
     <div className="countdown">
-      <span class="icon">
+      <span className="icon">
         <FaClock />
       </span>
 
-      <span class="time">
-        <span class="hours">10</span>
-        <span class="minutes">20</span>
-        <span class="seconds">30</span>
+      <span className="time">
+        <span className="hours">
+          {diff.hours().toString().padStart(2, "0")}
+        </span>
+        <span className="minutes">
+          {diff.minutes().toString().padStart(2, "0")}
+        </span>
+        <span className="seconds">
+          {diff.seconds().toString().padStart(2, "0")}
+        </span>
+        {" " + text}
       </span>
-   </div>
+    </div>
   )
 }
